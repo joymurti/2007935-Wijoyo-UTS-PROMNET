8cf64f0e2a30b6497eb10f2b2781213b69f36dde
joymurti <87194435+joymurti@users.noreply.github.com>
2021-11-04 15:54:40 +0700

    Update menu.php

--- UTS/JoyResto/menu.php
+++ UTS/JoyResto/menu.php
@@ -11,8 +11,11 @@
     if (isset($_GET['submit'])) {
         $sql = "SELECT * FROM menu WHERE nama LIKE '%".$_GET['search']."%' OR deskripsi LIKE '%".$_GET['search']."%'";
         $result = mysqli_query($link, $sql);
-    } else {
-        echo "Data Not Found!";
+        if (mysqli_num_rows($result) == 0) {
+            $show = "Data kosong !";
+        } else {
+            $show = "none";
+        }
     }
 ?>
 
@@ -63,10 +66,14 @@
                     <a href="delete.php?id=<?php echo $row['id_menu']; ?>"><button class="delete normal-button">Delete</button></a>
                 </td>
             </tr>
-
             <?php } ?>
         </tbody>
         </table>
+        <br>
+        <div class="alert" style="display: <?php echo $show ?>;">
+            <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+            <strong><?php echo "Data not found!"; ?></strong>
+        </div>
     </div>    
 
     <?php  include 'footer.php'; ?>
