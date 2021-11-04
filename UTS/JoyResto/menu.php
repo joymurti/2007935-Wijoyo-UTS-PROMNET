bb95141cec57c49c999f4644a2322471e6ca5efc
joymurti <87194435+joymurti@users.noreply.github.com>
2021-11-04 15:26:08 +0700

    Update menu.php

--- UTS/JoyResto/menu.php
+++ UTS/JoyResto/menu.php
@@ -7,16 +7,36 @@
     include 'config.php';
     $sql = "SELECT * FROM menu";
     $result = mysqli_query($link, $sql);
+
+    if (isset($_GET['submit'])) {
+        $sql = "SELECT * FROM menu WHERE nama LIKE '%".$_GET['search']."%' OR deskripsi LIKE '%".$_GET['search']."%'";
+        $result = mysqli_query($link, $sql);
+    } else {
+        echo "Data Not Found!";
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <?php  include 'head.php'; ?>
+<style>
+</style>
 <link rel="stylesheet" href="menu.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <body>
     <?php  include 'navbar.php'; ?>      
-    <div class="transparan" style="overflow-x:auto;">
-        <a href="add_menu.php" id="link-id"><button id="add-btn">+ Menu</button></a>
+    <div class="transparan" style="overflow-x:auto;">   
+        <div class="flex-row">
+            <div>
+                <a href="add_menu.php" id="link-id"><button id="add-btn" class="normal-button">+ Menu</button></a>
+            </div>
+            <div>
+                <form class="example" action="" method="GET">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button type="submit" name="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+        </div> 
         <br>
         <table>
         <thead>
@@ -38,9 +58,9 @@
                 <td><?php echo $row["harga"]; ?></td>
                 <td><img src="img/<?php echo $row['gambar']; ?>" alt="" srcset=""></td>
                 <td>
-                    <a href="crud_menu_edit.php?edit=<?php echo $row['id_menu']; ?>"><button class="update">Update</button></a>
+                    <a href="crud_menu_edit.php?edit=<?php echo $row['id_menu']; ?>"><button class="update normal-button">Update</button></a>
                     <br>
-                    <a href="delete.php?id=<?php echo $row['id_menu']; ?>"><button class="delete">Delete</button></a>
+                    <a href="delete.php?id=<?php echo $row['id_menu']; ?>"><button class="delete normal-button">Delete</button></a>
                 </td>
             </tr>
 
